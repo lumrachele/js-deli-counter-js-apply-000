e31435f33f46fd21af116e99228ab38c1b9ebcb5
Rachel Lum <rlum894@gmail.com>
2018-10-18 18:20:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function takeANumber (line, name){
-  var i =0
+  var i = 0
 for (let i = 0; i < line.length; i++) {
   line.push(`${name}`)
 return (`Welcome, ${name}. You are number ${i+1} in line.`)
